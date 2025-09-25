6db27b0bb5fd3a844c9d9f27790654221c605dd5
Salvatore Fendt <salvatore.fendt@mercedes-benz.com>
2025-09-25 15:21:28 +0200

    Improved UX by introducing menu panel and using ansi escape codes.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod types;
+mod console;
 
 use std::io::{Stdin, Stdout, Write};
 use crate::types::{parse_prebuild_pizza, parse_toppings, Pizza, Topping};
@@ -15,13 +16,22 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let prebuild_pizza_file_path ="pizza_prebuilds_text";
     let prebuild_content = fs::read_to_string(prebuild_pizza_file_path).expect("There has to be text!");
     let pizzas = parse_prebuild_pizza(&prebuild_content, &toppings)?;
-    
-    writeln!(stdout, "Welcome to Salvatores Pizza!")?;
-    writeln!(stdout, "1: Order Pizza")?;
-    writeln!(stdout, "2: Quit")?;
+
+    clear_screen(&mut stdout)?;
+
+    let title_text = String::from("Welcome to Salvatores Pizza!");
+
+    let panel = console::Menu::new(title_text, vec![
+        String::from("1: Order Pizza"),
+        String::from("2: Quit"),
+    ]);
+    writeln!(stdout, "{panel}")?;
+
 
     write!(stdout, "> ")?;
     stdout.flush()?;
+
+    clear_screen(&mut stdout)?;
 
     let mut input = String::new();
     stdin.read_line(&mut input)?;
@@ -36,11 +46,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn order_pizza(stdout: &mut Stdout, stdin: &Stdin, available_toppings: &Vec<Topping>, prebuild_pizzas: &Vec<Pizza>) -> Result<(), Box<dyn std::error::Error>> {
 
-    writeln!(stdout, "Pizza-Menu:")?;
+    let title_text = String::from("Pizza-Menu");
+    let mut menu_entries = Vec::<String>::new();
     for (index, pizza) in prebuild_pizzas.iter().enumerate() {
-        writeln!(stdout, "{}: {}", index, &pizza.name)?;
+        let pizza = &pizza.name;
+        menu_entries.push(format!("{index}: {pizza}"))
     }
-    writeln!(stdout, "C: Customize your own Pizza")?;
+    menu_entries.push(String::from("C: Customize your own Pizza"));
+    let panel = console::Menu::new(title_text, menu_entries);
+    writeln!(stdout, "{panel}")?;
 
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
@@ -54,7 +68,7 @@ fn order_pizza(stdout: &mut Stdout, stdin: &Stdin, available_toppings: &Vec<Topp
                         None => { writeln!(stdout, "unknown Menu-entry")?;}
                         Some(pizza) => {
                             let price: u32 = pizza.total_price();
-                            writeln!(stdout, "Your choice: {} for {}.00$", pizza.name, price)?;
+                            writeln!(stdout, "Your choice: \x1b[1;30m{}\x1b[0m for \x1b[1;30m{}.00$\x1b[0m.", pizza.name, price)?;
                         }
                     }
                 }
@@ -76,14 +90,18 @@ fn order_custom_pizza(stdout: &mut Stdout, stdin: &Stdin, available_toppings: &V
     };
 
     loop {
+        clear_screen(stdout)?;
 
-        writeln!(stdout, "Choose your toppings:")?;
+        let title_text = String::from("Choose your toppings");
+        let mut menu_entries = Vec::<String>::new();
 
         for topping in available_toppings.iter() {
             let shortname = topping.shortname();
             let name = &topping.name;
-            writeln!(stdout, "{shortname}: {name}")?;
+            menu_entries.push(format!("{shortname}: {name}"))
         }
+        let panel = console::Menu::new(title_text, menu_entries);
+        writeln!(stdout, "{panel}")?;
 
         writeln!(stdout, "Q: Quit")?;
 
@@ -105,9 +123,18 @@ fn order_custom_pizza(stdout: &mut Stdout, stdin: &Stdin, available_toppings: &V
             pizza.toppings.push(topping);
         }
         else {
-            writeln!(stdout, "Your toppings: {}", pizza.toppings.iter().map(|topping| topping.name.as_str()).collect::<Vec<&str>>().join(", "))?;
-            let price: u32 = pizza.total_price();
-            writeln!(stdout, "Your price: {}.00$", price)?;
+            clear_screen(stdout)?;
+            let title_text = String::from("Your toppings");
+            let mut topping_entries = Vec::<String>::new();
+
+            for topping in pizza.toppings.iter() {
+                topping_entries.push(format!("{}", topping.name))
+            }
+
+            let panel = console::Menu::new(title_text, topping_entries);
+            writeln!(stdout, "{panel}")?;
+            writeln!(stdout, "Your price: {}.00$", pizza.total_price())?;
+
             break;
         }
 
@@ -116,3 +143,7 @@ fn order_custom_pizza(stdout: &mut Stdout, stdin: &Stdin, available_toppings: &V
     Ok(())
 }
 
+fn clear_screen(stdout: &mut Stdout) -> Result<(), Box<dyn std::error::Error>> {
+    write!(stdout, "\x1B[2J\x1B[1;1H")?;
+    Ok(())
+}
