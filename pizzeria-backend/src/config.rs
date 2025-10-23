22282da5c2ea5cbf9ea56c2ab3751bd871ee5c94
Salvatore Fendt <salvatore.fendt@mercedes-benz.com>
2025-10-23 09:43:26 +0200

    HTTP Backend: Add generic implementation for extracting environment variables.

--- pizzeria-backend/src/config.rs
+++ pizzeria-backend/src/config.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::env::VarError;
-use std::net::IpAddr;
+use std::fmt::Display;
+use std::net::{IpAddr, Ipv4Addr};
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
@@ -10,39 +11,33 @@ pub struct Config {
 }
 
 const BIND_HOST_KEY: &str = "PIZZERIA_BACKEND_BIND_HOST";
+const BIND_HOST_DEFAULT: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
 const BIND_PORT_KEY: &str = "PIZZERIA_BACKEND_BIND_PORT";
+const BIND_PORT_DEFAULT: u16 = 3333;
 
 pub fn load_configuration_from_environment_variables() -> Result<Config, Box<dyn std::error::Error>> {
-    let bind_host = env::var(BIND_HOST_KEY);
+    Ok(Config {
+        bind_host: extract_environment_variable(BIND_HOST_KEY, BIND_HOST_DEFAULT)?,
+        bind_port: extract_environment_variable(BIND_PORT_KEY, BIND_PORT_DEFAULT)?
+    })
+}
 
-    let bind_host =  match bind_host {
-        Ok(bind_host) => IpAddr::from_str(&bind_host)?, //todo: add error context
+fn extract_environment_variable<T: FromStr>(key: &str, default: T) -> Result<T, Box<dyn std::error::Error>>
+where <T as FromStr>::Err: std::error::Error + 'static {
+    let value = env::var(key);
+
+    let value =  match value {
+        Ok(value) => T::from_str(&value)?, //todo: add error context
         Err(VarError::NotPresent) => {
-            IpAddr::from_str("127.0.0.1")
-                .expect("Should always be able to parse default IP address")
+            default
         }
         Err(error @ VarError::NotUnicode(_)) => {
             return Err(Box::new(error));
         }
     };
-
-    let bind_port = env::var(BIND_PORT_KEY);
-
-    let bind_port = match bind_port {
-        Ok(bind_port) => u16::from_str(&bind_port)?,    //todo: add error context
-        Err(VarError::NotPresent) => {
-            3333
-        }
-        Err(error @ VarError::NotUnicode(_)) => {
-            return Err(Box::new(error))
-        }
-    };
-
-    Ok(Config {
-        bind_host,
-        bind_port,
-    })
+    Ok(value)
 }
+
 
 #[cfg(test)]
 mod tests {
