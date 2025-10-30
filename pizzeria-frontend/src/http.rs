4e8ce4c09ad7c4aff3feb8b966e9e2684337a0d2
Salvatore Fendt <salvatore.fendt@mercedes-benz.com>
2025-10-30 17:23:17 +0100

    HTTP Frontend: Introduced `format_http_request` to generalize HTTP requests.

--- pizzeria-frontend/src/http.rs
+++ pizzeria-frontend/src/http.rs
@@ -45,17 +45,25 @@ pub fn backend_socket_addr() -> Result<SocketAddr, FrontendError> {
     Ok(SocketAddr::new(host, port))
 }
 
+fn format_http_request(
+    method: &str,
+    path: &str,
+    address: SocketAddr
+) -> String {
+    format!("{method} {path} HTTP/1.1\r
+Host: {address}\r
+\r
+")
+}
+
 pub fn read_pizza_prebuilds() -> io::Result<String> {
     let addr = backend_socket_addr()
         .map_err(FrontendError::into_io)?;
     let mut stream = TcpStream::connect(addr)?;
+    let method = "GET";
+    let path = "/";
 
-    write!(stream, "GET / HTTP/1.1\r
-Host: {addr}\r
-User-Agent: curl/8.5.0\r
-Accept: */*\r
-\r
-")?;
+    write!(stream, "{}", format_http_request(method, path, addr))?;
     stream.flush()?;
 
     let body = parse_http_response_body(stream)
@@ -67,13 +75,10 @@ pub fn read_toppings() -> io::Result<String> {
     let addr = backend_socket_addr()
         .map_err(FrontendError::into_io)?;
     let mut stream = TcpStream::connect(addr)?;
+    let method = "GET";
+    let path = "/toppings";
 
-    write!(stream, "GET /toppings HTTP/1.1\r
-Host: {addr}\r
-User-Agent: curl/8.5.0\r
-Accept: */*\r
-\r
-")?;
+    write!(stream, "{}", format_http_request(method, path, addr))?;
     stream.flush()?;
 
     let body = parse_http_response_body(stream)
@@ -86,6 +91,7 @@ pub fn send_transaction_record(transaction_record: String) -> io::Result<()> {
         .map_err(FrontendError::into_io)?;
     let mut stream = TcpStream::connect(addr)?;
     let transaction_record_length = transaction_record.len();
+
 
     stream.write_all(format!("POST /transaction HTTP/1.1\r
 Host: {addr}\r
@@ -172,6 +178,8 @@ mod tests {
     use crate::transactions::format_transaction_as_string;
     use std::io::Cursor;
     use std::{env, fs};
+    use std::error::Error;
+    use std::str::FromStr;
     use tempfile::tempdir;
 
     #[test]
@@ -217,5 +225,22 @@ body-text");
 
         // 6) CWD zurück
         env::set_current_dir(old).unwrap();
+    }
+
+    #[test]
+    fn should_format_http_request() -> Result<(), Box<dyn Error>> {
+        let method = "GET";
+        let path = "/";
+        let address= SocketAddr::from_str("127.0.0.1:3333")?;
+
+        let actual_request = format_http_request(method, path, address);
+
+        let expected_request = "GET / HTTP/1.1\r
+Host: 127.0.0.1:3333\r
+\r
+";
+
+        assert_eq!(actual_request, expected_request);
+        Ok(())
     }
 }
